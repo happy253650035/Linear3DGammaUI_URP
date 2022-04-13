ef3bab4cbb8fc996c79331f499fa6c2d4c3f11af
yinyi <yinyi@kuaishou.com>
2022-04-13 18:09:14 +0800

    init

---
+++ Assets/_GammaUIFix/GammaUIFix.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[CreateAssetMenu]
+public class GammaUIFix : ScriptableRendererFeature
+{
+    public Material material;
+    
+	public GammaUIFix()
+	{
+	}
+
+	public override void Create()
+	{
+	}
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        var cameraColorTarget = renderer.cameraColorTarget;
+        var DrawUIIntoRTPass = new DrawUIIntoRTPass(RenderPassEvent.BeforeRenderingTransparents, cameraColorTarget);
+        var BlitRenderPassesToScreen = new BlitPass(RenderPassEvent.AfterRenderingTransparents, cameraColorTarget,material);
+
+        renderer.EnqueuePass(DrawUIIntoRTPass);
+        renderer.EnqueuePass(BlitRenderPassesToScreen);
+    }
+
+    //-------------------------------------------------------------------------
+
+	class DrawUIIntoRTPass : ScriptableRenderPass
+	{
+        private RenderTargetIdentifier colorHandle;
+
+        //The temporary UI texture
+        public static int m_uiRTid = Shader.PropertyToID("_UITexture");
+        public static RenderTargetIdentifier m_uiRT = new RenderTargetIdentifier(m_uiRTid);
+
+        public DrawUIIntoRTPass(RenderPassEvent renderPassEvent, RenderTargetIdentifier colorHandle)
+        {
+            this.colorHandle = colorHandle;
+            this.renderPassEvent = renderPassEvent;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescripor)
+        {
+            RenderTextureDescriptor descriptor = cameraTextureDescripor;
+            descriptor.colorFormat = RenderTextureFormat.Default;
+            cmd.GetTemporaryRT(m_uiRTid, descriptor);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+			CommandBuffer cmd = CommandBufferPool.Get("Draw UI Into RT Pass");
+
+            cmd.SetRenderTarget(m_uiRT);
+            cmd.ClearRenderTarget(true,true,Color.clear);
+
+            context.ExecuteCommandBuffer(cmd);
+			CommandBufferPool.Release(cmd);
+		}
+
+		public override void FrameCleanup(CommandBuffer cmd)
+		{
+			if (cmd == null)
+				throw new ArgumentNullException("cmd");
+
+			base.FrameCleanup(cmd);
+		}
+	}
+
+    //-------------------------------------------------------------------------
+
+	class BlitPass : ScriptableRenderPass
+	{
+        private RenderTargetIdentifier colorHandle;
+        Material material;
+
+        public BlitPass(RenderPassEvent renderPassEvent, RenderTargetIdentifier colorHandle, Material mat)
+        {
+            this.colorHandle = colorHandle;
+            this.renderPassEvent = renderPassEvent;
+            this.material = mat;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+			CommandBuffer cmd = CommandBufferPool.Get("Blit Pass");
+            
+            cmd.Blit(DrawUIIntoRTPass.m_uiRT,colorHandle,material);
+
+            context.ExecuteCommandBuffer(cmd);
+			CommandBufferPool.Release(cmd);
+		}
+
+		public override void FrameCleanup(CommandBuffer cmd)
+		{
+			if (cmd == null)
+				throw new ArgumentNullException("cmd");
+
+            cmd.ReleaseTemporaryRT(DrawUIIntoRTPass.m_uiRTid);
+
+			base.FrameCleanup(cmd);
+		}
+	}
+
+    //-------------------------------------------------------------------------
+}
---
+++ Assets/_GammaUIFix/GammaUIFix.shader
@@ -0,0 +1,62 @@
+﻿Shader "GammaUIFix"
+{
+    Properties
+    {
+        [HideInInspector] _MainTex ("UI Texture", 2D) = "white" {}
+        //_UITex ("_UITex", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _CameraColorTexture;
+            sampler2D _MainTex;
+
+            float4 frag (v2f i) : SV_Target
+            {
+                float4 uicol = tex2D(_MainTex, i.uv); //ui in lighter color
+                uicol.a = LinearToGammaSpace(uicol.a); //make ui alpha in lighter color
+
+                float4 col = tex2D(_CameraColorTexture, i.uv); //3d in normal color
+                col.rgb = LinearToGammaSpace(col.rgb); //make 3d in lighter color
+
+                float4 result;
+                result.rgb = lerp(col.rgb,uicol.rgb,uicol.a); //do linear blending
+                result.rgb = GammaToLinearSpace(result.rgb); //make result normal color
+                result.a = 1;
+
+                return result;
+            }
+            ENDCG
+        }
+    }
+}
